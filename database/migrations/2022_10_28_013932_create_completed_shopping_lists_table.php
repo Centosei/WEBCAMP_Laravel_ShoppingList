08a87b646691dba78cdac790613cb66101db4043
Riki <ec2-user@ip-172-31-65-45.ec2.internal>
2022-10-28 01:52:19 +0000

    set up

---
+++ database/migrations/2022_10_28_013932_create_completed_shopping_lists_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompletedShoppingListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('completed_shopping_lists', function (Blueprint $table) {
+            $table->unsignedInteger('id');
+            $table->string('name', 255)->comment('買うもの名');
+            $table->unsignedBigInteger('user_id')->comment('登録したユーザー');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->dateTime('created_at')->useCurrent()->comment('購入日');
+            $table->dateTime('updated_at')->useCurrent()->useCurrentOnUpdate();
+            //
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('completed_shopping_lists');
+    }
+}
